dc82ed6e648226ca4a77b906c017e14ee3017033
Malte <malte.luttermann@student.uni-luebeck.de>
2019-12-23 21:24:32 +0100

    Added some communication

--- action.php
+++ action.php
@@ -1,5 +1,7 @@
 <?php
 
+$DATA_FILE = __DIR__ . '/data/entries.json';
+
 handle_request();
 
 /**
@@ -10,34 +12,60 @@ function handle_request() {
 		die;
 	}
 
+	$list = isset($_POST['list']) && is_numeric($_POST['list']) && strlen($_POST['list']) > 5 ? $_POST['list'] : 0;
+
 	switch($_POST['type']) {
-		case 'onload': // Load site
-			$data = load_data();
-			echo $data;
+		case 'onload': // Load watchlist data
+			echo load_data($list);
 			break;
 		case 'add': // Create new entry
 			try {
-				echo add_entry(); // echo true if success, else false
+				echo add_entry($list); // echo true if success, else false
 			} catch (Exception $e) {
 				echo false;
 			}
+			break;
+		case 'state': // Load DOM state
+			echo load_lists();
 			break;
 	}
 }
 
 /**
+ * Loads all available lists.
+ */
+function load_lists() {
+	$file = __DIR__ . '/data/lists.json';
+	if (!file_exists($file)) {
+		file_put_contents($file, json_encode(array(array('Default watchlist' => 0)), JSON_PRETTY_PRINT));
+	}
+	
+	return json_encode(file_get_contents($file));
+}
+
+/**
  * Loads available data.
  * @param int list ID of the list which data should be loaded.
+ * @return array Array of JSON objects for each entry.
  */
 function load_data($list) {
+	global $DATA_FILE;
 
+	if (!file_exists($DATA_FILE)) {
+		return json_encode(array());
+	} else {
+		return json_encode(file_get_contents($DATA_FILE));
+	}
 }
 
 /**
  * Adds a new entry to the watchlist.
- * @return bool True if entry was added successfully, else false. 
+ * @param int list ID of the list to which data should be added.
+ * @return bool True if entry was added successfully, else false.
  */
-function add_entry() {
+function add_entry($list) {
+	global $DATA_FILE;
+
 	if (!isset($_POST['from']) || !isset($_POST['to'])
 		|| !isset($_POST['depart']) || !isset($_POST['return'])
 		|| !isset($_POST['cabin']) || !isset($_POST['travellers'])
@@ -47,8 +75,8 @@ function add_entry() {
 
 	if (!is_string($_POST['from']) || preg_match('/[^a-zA-Z]/', $_POST['from']) || strlen($_POST['from']) > 50
 		|| !is_string($_POST['to']) || preg_match('/[^a-zA-Z]/', $_POST['to']) || strlen($_POST['to']) > 50
-		|| !is_string($_POST['depart']) || !preg_match('/\d\d\/\d\d\/\d\d\d\d/', $_POST['depart'])
-		|| !is_string($_POST['return']) || !preg_match('/\d\d\/\d\d\/\d\d\d\d/', $_POST['return'])
+		|| !is_string($_POST['depart']) || !preg_match('/\d\d\d\d-\d\d-\d\d/', $_POST['depart'])
+		|| !is_string($_POST['return']) || !preg_match('/\d\d\d\d-\d\d-\d\d/', $_POST['return'])
 		|| !is_string($_POST['cabin']) || !in_array($_POST['cabin'], array('Economy', 'Premium Economy', 'Business class', 'First class'))
 		|| !is_numeric($_POST['travellers']) || preg_match('/[^0-9]/', $_POST['travellers']) || strlen($_POST['travellers']) > 3
 	) {
@@ -59,6 +87,7 @@ function add_entry() {
 	$data = array(
 		'id' => sha1($timestamp),
 		'date' => strval($timestamp),
+		'list' => $list,
 		'from' => $_POST['from'],
 		'to' => $_POST['to'],
 		'depart' => $_POST['depart'],
@@ -67,15 +96,14 @@ function add_entry() {
 		'travellers' => $_POST['travellers']
 	);
 
-	$file = __DIR__ . '/data/entries.json';
-	if (!file_exists($file)) {
+	if (!file_exists($DATA_FILE)) {
 		$content = array();
 	} else {
-		$content = json_decode(file_get_contents($file), true);
+		$content = json_decode(file_get_contents($DATA_FILE), true);
 	}
 
 	$content[] = $data;
-	file_put_contents($file, json_encode($content, JSON_PRETTY_PRINT));
+	file_put_contents($DATA_FILE, json_encode($content, JSON_PRETTY_PRINT));
 
 	return true;
 }
--- client/client.js
+++ client/client.js
@@ -2,23 +2,48 @@
 	'use strict';
 
 	$('#input-depart').datepicker({
-		format: 'mm/dd/yyyy',
+		format: 'yyyy-mm-dd',
 		todayHighlight: true,
 		autoclose: true
 	});
 
 	$('#input-return').datepicker({
-		format: 'mm/dd/yyyy',
+		format: 'yyyy-mm-dd',
 		todayHighlight: true,
 		autoclose: true
 	});
 
 	// SERVERURL is defined in the environment and written by the php script
-	// $.post(SERVERURL + '/action.php', 'type=onload', data => {
-	// 	show_message(data);
-	// });
+	$.post(SERVERURL + '/action.php', 'type=state', data => {
+		update_state(JSON.parse(data));
+	});
+
+	$.post(SERVERURL + '/action.php', 'type=onload', data => {
+		show_watchlist(JSON.parse(data));
+	});
 
 })();
+
+/**
+ * Loads the DOM state.
+ * @param {array} data Array of JSON objects containing the DOM state represenation.
+ */
+function update_state(data) {
+	console.log(data)
+	for (list of JSON.parse(data)) {
+		let keys = Object.keys(list);
+		$('#sel-list1').append('<option>', {value: list[keys[0]], text: keys[0]});
+		$('#sel-list2').append('<option>', {value: list[keys[0]], text: keys[0]});
+	}
+}
+
+/**
+ * Displays the watchlist.
+ * @param {array} data Array of JSON objects for each watchlist entry.
+ */
+function show_watchlist(data) {
+	$('#div-watchlist').html(JSON.parse(data));
+}
 
 /**
  * Hides information messages.
