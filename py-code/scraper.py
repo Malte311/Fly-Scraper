75171e31517396f0bafbb79d89df09b97e8a581d
Malte <malte.luttermann@student.uni-luebeck.de>
2020-02-26 14:22:02 +0100

    Setting origin and destination

--- py-code/scraper.py
+++ py-code/scraper.py
@@ -7,36 +7,27 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+SLEEP_TIME = 0.4
+
 def get_info(flight):
-	url = 'https://www.google.de/flights#flt=/m/03hrz.FNC.2020-05-03*FNC./m/03hrz.2020-05-31;c:EUR;e:1;sd:1;t:f'
 	chrome_options = Options()
 	#chrome_options.add_argument("--headless")
 	driver = webdriver.Chrome(executable_path='drivers/chromedriver', options=chrome_options)
 
 	try:
-		driver.get(url)
+		driver.get('https://www.google.de/flights')
 
-		set_travellers(driver, int(flight['travellers']))
+		#set_travellers(driver, int(flight['travellers']))
+		#set_cabin(driver, flight['cabin'])
+		#set_from(driver, flight['from'])
+		#set_to(driver, flight['to'])
+		set_depart(driver, flight['depart'])
+		#set_return(driver, flight['return'])
 
 		print("Sleeping")
 		time.sleep(120)
 
-		set_cabin(driver, flight['cabin'])
-
-		results = []
-		flights = driver.find_elements_by_css_selector('.gws-flights-results__itinerary-card')
-		for flightRes in flights:
-			print(flightRes.text)
-			resObj = {}
-			data = re.split('[\n\r]+', flightRes.text)
-			resObj['airline'] = data[1]
-			resObj['duration'] = data[2]
-			resObj['stops'] = data[4]
-			resObj['stay'] = data[5]
-			resObj['price'] = data[6]
-			results.append(resObj)
-
-		put_info(flight, results)
+		put_info(flight, fetch_results(driver))
 		
 	except Exception:
 		put_info(flight, {}) # mark as not successful
@@ -47,32 +38,32 @@ def get_info(flight):
 def set_travellers(driver, travellers):
 	menu = driver.find_elements_by_css_selector('.gws-flights-form__menu-label')
 
-	time.sleep(0.2)
+	time.sleep(SLEEP_TIME)
 	menu[1].click() # Number of travellers
 
-	time.sleep(0.2)
+	time.sleep(SLEEP_TIME)
 	inc = driver.find_element_by_css_selector('.gws-flights-widgets-numberpicker__flipper-increment')
 	for _i in range(travellers - 1):
 		parent = inc.find_element_by_xpath('..')
 		parent.click()
-		time.sleep(0.4)
+		time.sleep(SLEEP_TIME)
 	
-	time.sleep(0.2)
+	time.sleep(SLEEP_TIME)
 	driver.find_element_by_css_selector('.gws-flights__dialog-primary-button').click()
 
-	time.sleep(0.2)
+	time.sleep(SLEEP_TIME)
 
 def set_cabin(driver, cabin_str):
 	menu = driver.find_elements_by_css_selector('.gws-flights-form__menu-label')
 
-	time.sleep(0.2)
+	time.sleep(SLEEP_TIME)
 	menu[2].click() # Cabin class
 
-	time.sleep(0.2)
+	time.sleep(SLEEP_TIME)
 	cabin = driver.find_elements_by_css_selector('.X4hwq.flt-subhead2')		
 	cabin[map_cabin(cabin_str)].click()
 
-	time.sleep(0.2)
+	time.sleep(SLEEP_TIME)
 
 def map_cabin(cabin_str):
 	if 'first class' in cabin_str.lower():
@@ -83,6 +74,73 @@ def map_cabin(cabin_str):
 		return 1
 	else:
 		return 0
+
+def set_from(driver, origin_airport):
+	airport = driver.find_element_by_css_selector('.gws-flights-form__input-target')
+	parent = airport.find_element_by_xpath('..')
+
+	time.sleep(SLEEP_TIME)
+	parent.click()
+
+	time.sleep(SLEEP_TIME)
+	input_field = driver.find_element_by_id('sb_ifc50').find_element_by_xpath(".//*")
+	time.sleep(SLEEP_TIME)
+	input_field.clear()
+	time.sleep(SLEEP_TIME)
+	input_field.send_keys(origin_airport)
+
+	time.sleep(SLEEP_TIME)
+	airport_result = driver.find_element_by_css_selector('.fsapp-option-content')
+	time.sleep(SLEEP_TIME)
+	airport_result.click()
+	
+	time.sleep(SLEEP_TIME)
+
+def set_to(driver, dest_airport):
+	airport = driver.find_elements_by_css_selector('.gws-flights-form__input-target')[1]
+	parent = airport.find_element_by_xpath('..')
+
+	time.sleep(SLEEP_TIME)
+	parent.click()
+
+	time.sleep(SLEEP_TIME)
+	input_field = driver.find_element_by_id('sb_ifc50').find_element_by_xpath(".//*")
+	time.sleep(SLEEP_TIME)
+	input_field.clear()
+	time.sleep(SLEEP_TIME)
+	input_field.send_keys(dest_airport)
+
+	time.sleep(SLEEP_TIME)
+	airport_result = driver.find_element_by_css_selector('.fsapp-option-content')
+	time.sleep(SLEEP_TIME)
+	airport_result.click()
+
+	time.sleep(SLEEP_TIME)
+
+def set_depart(driver, depart_date):
+	datepicker = driver.find_element_by_css_selector('.gws-flights-form__input-target')
+	parent = datepicker.find_element_by_xpath('..')
+	parent.click()
+	time.sleep(SLEEP_TIME)
+
+def set_return(driver, return_date):
+	time.sleep(SLEEP_TIME)
+
+def fetch_results(driver):
+	results = []
+	flights = driver.find_elements_by_css_selector('.gws-flights-results__itinerary-card')
+	for flightRes in flights:
+		print(flightRes.text)
+		resObj = {}
+		data = re.split('[\n\r]+', flightRes.text)
+		resObj['airline'] = data[1]
+		resObj['duration'] = data[2]
+		resObj['stops'] = data[4]
+		resObj['stay'] = data[5]
+		resObj['price'] = data[6]
+		results.append(resObj)
+
+	return results
 
 def put_info(flight, info):
 	file_name = path_prefix() + str(flight['id']) + '.json'
